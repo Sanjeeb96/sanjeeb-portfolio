6ec6ad2bfa0dbf14b4d11978078a489454678af5
Sanjeeb Padhi <sanjeebpadhi96@gmail.com>
2023-08-31 23:50:26 +0530

    files uploaded

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <div className=""> 3D developer portfolio</div>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,32 @@
+import { motion } from "framer-motion";
+
+import { ComputersCanvas } from "./canvas";
+import { styles } from "@/app/styles";
+
+const Hero = () => {
+  return (
+    <section className="relative w-full h-screen mx-auto">
+      <div className="sm:px-16 px-6 absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5">
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5  rounded-full bg-[#915eff]" />
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+        </div>
+
+        <div>
+          <h1 className="font-black text-white lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2">
+            Hi, I'm <span className="text-[#915eff]">Sanjeeb</span>
+          </h1>
+          <p className="text-[#dfd9ff] font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px] mt-2">
+            I design & develop user interfaces,
+            <br className="sm:block hidden" />
+            user experiance & web application.
+          </p>
+        </div>
+      </div>
+
+      <ComputersCanvas />
+    </section>
+  );
+};
+
+export default Hero;
