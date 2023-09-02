4c7e5dd989bc79e908c871a736c18e8873ceeeb8
Sanjeeb Padhi <sanjeebpadhi96@gmail.com>
2023-09-02 23:43:27 +0530

    3d ball section completed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,12 +1,42 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable react-refresh/only-export-components */
 
 import { motion } from "framer-motion";
+import { Tilt } from "react-tilt";
 
 import { styles } from "../styles";
-import { services } from "../constants";
 import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
-import ServiceCard from "./ServiceCard";
+import { services } from "../constants";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className="xs:w-[250px] w-full">
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className=" w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+    >
+      <div
+        // eslint-disable-next-line react/no-unknown-property
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+      >
+        <img
+          src={icon}
+          alt="web-development"
+          className="w-16 h-16 object-contain"
+        />
+
+        <h3 className="text-white text-[20px] font-bold text-center">
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
 
 const About = () => {
   return (
@@ -20,11 +50,11 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
       >
-        I &apos m a skilled software developer with experience in TypeScript and
+        I&apos;m a skilled software developer with experience in TypeScript and
         JavaScript, and expertise in frameworks like React, Node.js, and
-        Three.js. I &apos m a quick learner and collaborate closely with clients
+        Three.js. I&apos;m a quick learner and collaborate closely with clients
         to create efficient, scalable, and user-friendly solutions that solve
-        real-world problems. Let &apos s work together to bring your ideas to
+        real-world problems. Let&apos;s work together to bring your ideas to
         life!
       </motion.p>
 
